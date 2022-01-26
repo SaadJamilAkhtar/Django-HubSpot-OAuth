023dd024b00e390a2343cffabc35c4dffbdeac59
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-01-26 17:25:55 +0500

    updated models

---
+++ HubSpot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HubspotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HubSpot'
---
+++ HubSpot/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class HubSpotCredentials(models.Model):
+    client_id = models.CharField(max_length=255)
+    client_secret = models.CharField(max_length=500)
+    redirect_url = models.CharField(max_length=255)
+
+
+class HubSpotToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    refresh_token = models.CharField(max_length=500)
+    access_token = models.CharField(max_length=500)
